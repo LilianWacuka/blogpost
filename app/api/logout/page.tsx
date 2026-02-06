55e1a957d1face973e5258ac3f877dbd75923166
lilian <lilianguruw@gmail.com>
2026-02-06 14:33:18 +0300

    log in issues

--- app/api/logout/page.tsx
+++ app/api/logout/page.tsx
@@ -3,7 +3,8 @@ import { NextResponse } from "next/server";
 export async function POST() {
     // Clear the token cookie 
     const response = NextResponse.json({ message: "logged out successfully"})
-        localStorage.removeItem("token");
-    
+    response.cookies.set("token", "",
+        {path:'/', expires: new Date(0) }
+    );
     return response;
 }
